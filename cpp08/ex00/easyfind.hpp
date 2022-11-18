383319947e34a446cf9cd54add86da8bd7e14c63
jferrer-l <jferrerluengo@gmail.com>
2022-11-18 20:11:41 +0100

    ex00

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -6,16 +6,29 @@
 /*   By: jferrer- <jferrer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 17:14:08 by jferrer-          #+#    #+#             */
-/*   Updated: 2022/11/15 20:43:16 by jferrer-         ###   ########.fr       */
+/*   Updated: 2022/11/18 19:45:22 by jferrer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <algorithm>
+#include <type_traits>
+#include <vector>
 
-#ifndef EASYFIND_HPP
-#define EASYFIND_HPP
+class NotFound: public std::exception
+{
+	virtual const char* what() const throw()
+	{
+		return ("Element not found");
+	}
+};
 
-template <typename T>
-e
 
-#endif
+template < template < typename , typename> class T>
+typename T<int, std::allocator<int> >::iterator
+easyfind(T<int, std::allocator<int> > &type, int i)
+{
+	typename T<int, std::allocator<int> >::iterator n = std::find(type.begin(), type.end(), i);
+	return ( n == type.end() ? throw NotFound() : n);
+}
+
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,45 @@
 /*   By: jferrer- <jferrer-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 17:14:04 by jferrer-          #+#    #+#             */
-/*   Updated: 2022/11/15 17:14:04 by jferrer-         ###   ########.fr       */
+/*   Updated: 2022/11/18 20:10:36 by jferrer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+
+int main()
+{
+	std::vector<int> vec;
+
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(4);
+	vec.push_back(3);
+	vec.push_back(5);
+	vec.push_back(0);
+	vec.push_back(5);
+	std::cout << *easyfind(vec, 4) << std::endl;
+	std::cout << *easyfind(vec, 5) << std::endl;
+
+	try { std::cout << *easyfind(vec, 42) << std::endl; }
+	catch(const std::exception& e) { std::cerr << e.what() << '\n'; }
+
+	std::list<int> lis;
+
+	lis.push_back(3);
+	lis.push_back(2);
+	lis.push_back(5);
+	lis.push_back(4);
+	lis.push_back(10);
+
+	std::cout << *easyfind(lis, 3) << std::endl;
+	std::cout << *easyfind(lis, 10) << std::endl;
+
+	try { std::cout << *easyfind(lis, 42) << std::endl; }
+	catch(const std::exception& e) { std::cerr << e.what() << '\n'; }
+
+	return (0);
+}
